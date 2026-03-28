c202b28250c7afaeffdf93addc27be77dc7ac713
ravikumargupta833-dev <ravikumargupta833@gmail.com>
2026-03-29 02:25:17 +0530

    Script 5.sh

--- Script 5.sh
+++ Script 5.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 # Name: Ravi Kumar Gupta
-# Registration Number: 24BCE11274
-# Script: Open Source Manifest Generator
+# Script 5: Open Source Manifest Generator
 
 echo "Answer a few questions..."
+echo ""
 
-read -p "Which tool do you use daily? " tool
-read -p "What does freedom mean to you? " freedom
-read -p "What do you want to build in future? " goal
+read -p "Which tool do you use daily? " TOOL
+read -p "What does freedom mean to you? " FREEDOM
+read -p "What do you want to build in future? " BUILD
 
-username=$(whoami)
-date_now=$(date +%Y-%m-%d)
+DATE=$(date '+%d %B %Y')
+OUTPUT="manifest_$(whoami).txt"
 
-filename="manifest_${username}.txt"
-
-# Creating paragraph
-echo "On $date_now, I reflected on my usage of open tools." > $filename
-echo "I regularly use $tool, which plays an important role in my daily work." >> $filename
-echo "For me, freedom means $freedom." >> $filename
-echo "In the future, I want to build $goal using open-source technologies." >> $filename
-echo "This shows how open-source connects learning and creativity." >> $filename
+# Generate manifesto
+echo "On $DATE, I reflected on open-source tools." > $OUTPUT
+echo "I regularly use $TOOL, which helps me in my daily work." >> $OUTPUT
+echo "For me, freedom means $FREEDOM." >> $OUTPUT
+echo "In future, I want to build $BUILD and share it openly." >> $OUTPUT
+echo "Open source connects learning, creativity, and collaboration." >> $OUTPUT
 
 echo ""
-echo "Manifest saved in $filename"
-echo "----- Content -----"
-cat $filename
+echo "Manifest saved to $OUTPUT"
+echo "------ Content ------"
+cat $OUTPUT
