69c50e2bcb52a65025b659f2152b75c7e4d9b7c6
ravikumargupta833-dev <ravikumargupta833@gmail.com>
2026-03-31 23:04:06 +0530

    Update Script 5.sh

--- Script 5.sh
+++ Script 5.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-# Script 5: Open Source Manifesto Generator
+
 # Name: Ravi Kumar Gupta
-# Reg No: 24BCE11274
+# Registration Number: 24BCE11274
+# Script: Open Source Manifest Generator
 
-echo "Answer a few questions to generate your manifesto"
+echo "Answer a few questions..."
+
+read -p "Which tool do you use daily? " tool
+read -p "What does freedom mean to you? " freedom
+read -p "What do you want to build in future? " goal
+
+username=$(whoami)
+date_now=$(date +%Y-%m-%d)
+
+filename="manifest_${username}.txt"
+
+# Creating paragraph
+echo "On $date_now, I reflected on my usage of open tools." > $filename
+echo "I regularly use $tool, which plays an important role in my daily work." >> $filename
+echo "For me, freedom means $freedom." >> $filename
+echo "In the future, I want to build $goal using open-source technologies." >> $filename
+echo "This shows how open-source connects learning and creativity." >> $filename
+
 echo ""
-
-read -p "1. Tool you use daily: " TOOL
-read -p "2. What does freedom mean to you? " FREEDOM
-read -p "3. What would you build openly? " BUILD
-
-DATE=$(date '+%d %B %Y')
-OUTPUT="manifesto_24BCE11274.txt"
-
-echo "--------------------------------" > $OUTPUT
-echo "Open Source Manifesto" >> $OUTPUT
-echo "Date: $DATE" >> $OUTPUT
-echo "" >> $OUTPUT
-
-echo "I regularly use $TOOL in my daily work. For me, freedom means $FREEDOM." >> $OUTPUT
-echo "In the future, I would like to build $BUILD and share it openly so others can learn and improve it." >> $OUTPUT
-
-echo ""
-echo "Manifesto saved in $OUTPUT"
-cat $OUTPUT
+echo "Manifest saved in $filename"
+echo "----- Content -----"
+cat $filename
